9e882e6d8ba80c2118280b75fadc39ccae7ba493
AWrongUsername <naimverboom@gmail.com>
2020-05-17 23:46:09 +0200

    Init Commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Home - Naim Verboom</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300&family=Montserrat:wght@300;500&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="nav">
+		<ul>
+			<li id="right"><a href="/programming.php">Programming</a></li>
+			<li id="right"><a href="/photography">Photography</a></li>
+			<li><a href="/">Naim Verboom</a></li>
+		</ul>
+	</div>
+
+	<div class="body">
+		<div class="head">
+			<h1 class="header">Naim Verboom</h1>
+			<h2 class="subheader">Creating My Mind</h2>
+			<p class=headtext>Making programs and websites for myself and others.</p>
+		</div>
+
+		<div class="content">
+			<h1 class="title">Projects</h1>
+			<h2 class="subtitle">What am I proud of?</h2>
+
+			<div class="showoff">
+				<div class="show-article">
+					<a href="http://weather.naimverboom.nl"><h2 class="show-title">Weather Application</h2></a>
+					<a href="http://weather.naimverboom.nl"><img src="../img/Weatherapp.png" alt="Weather App Image" class="show-img"></a>
+					<p class="show-descr">Simple weather app using Node.JS and various web-api's.</p>
+				</div>
+				<div class="show-article">
+					<a href="http://drmhouse.tk"><h2 class="show-title">DRMHouse</h2></a>
+					<a href="http://drmhouse.tk"><img src="../img/Drmhouse.png" alt="Drmhouse image" class="show-img"></a>
+					<p class="show-descr">Website to sell and buy houses using PHP, HTML and CSS.</p>
+				</div>
+
+				
+			</div>
+			<div class="footer">
+				<p class="madeby">Website made by Naim Verboom</p>
+				<p class="contact">Contact:</p>
+				<div class="contactlist">
+					<a href="mailto:naimverboom@gmail.com">naimverboom@gmail.com</a>
+					<a>Discord: AWrongUsername#9129</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
